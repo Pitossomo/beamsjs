9e4bdf9cbc8107ba5c4a5f84322ba7f106dd49d1
pitossomo <pedro.h.teles@hotmail.com>
2022-08-31 16:46:19 -0300

    Distributed load implemented methods for finding total and partial centroid, force and values

--- src/classes/DistributedLoad.ts
+++ src/classes/DistributedLoad.ts
@@ -26,26 +26,32 @@ export class DistributedLoad implements iDistributedLoad {
 
     const length = xf - x0
     this.tg = (endValue - startValue)/length || 0
-    this.xCentroid = (2*startValue + endValue)*length/(3*(startValue+endValue))
+    this.xCentroid = x0 + (2*endValue + startValue)*length/(3*(startValue+endValue))
     this.totalForce = (startValue + endValue)/2*length
 
-    this.valueX = x => (x < x0 || x > xf) ? 0 : startValue + this.tg*x
+    this.valueX = x => (x < this.x0 || x > this.xf) ? 0 : this.startValue + this.tg*(x-this.x0)
     
     this.partialCentroid = (x0, xf) => {
       if (xf <= this.x0 || x0 >= this.xf) return 0
       if (x0 <= this.x0 && xf >= this.xf) return this.xCentroid
-
+      
       const newX0 = Math.max(x0, this.x0)
       const newXf = Math.min(xf, this.xf) 
+      const partialLength = newXf - newX0
 
       const [valueX0, valueXf] = [this.valueX(newX0), this.valueX(newXf)] 
-      return (2*valueX0 + valueXf)*length/(3*(valueX0+valueXf))
+      return newX0 + partialLength*(valueX0+2*valueXf)/(3*(valueX0+valueXf))
     }
 
     this.partialForce = (x0, xf) => {
-      if (x0 <= this.x0) return 0
-      if (xf >= this.xf) return this.totalForce
-      return (this.valueX(x0) + this.valueX(xf))*length/2
+      if (xf <= this.x0 || x0 >= this.xf) return 0
+      if (x0 <= this.x0 && xf >= this.xf) return this.totalForce
+
+      const newX0 = Math.max(x0, this.x0)
+      const newXf = Math.min(xf, this.xf) 
+      const partialLength = newXf - newX0
+
+      return (this.valueX(x0) + this.valueX(xf))*partialLength/2
     } 
   }
 }
--- src/tests/DistributedLoad.test.ts
+++ src/tests/DistributedLoad.test.ts
@@ -1,9 +1,69 @@
 import { DistributedLoad } from "../classes/DistributedLoad";
 
-describe("DistributedLoad", () => {
-  const load = new DistributedLoad(19, 49, 2, 8)
+describe("Unitary Trapeze Distributed Load", () => {
+  const load = new DistributedLoad(0, 1, 0, 1)
+
+  it('calculates partial values correctly', () => {
+    expect(load.valueX(-1)).toEqual(0)
+    expect(load.valueX(0)).toEqual(0)
+    expect(load.valueX(0.5)).toEqual(0.5)
+    expect(load.valueX(1)).toEqual(1)
+    expect(load.valueX(2)).toEqual(0)
+  })
+
   it('calculates partial centroid correctly', () => {
     expect(load.partialCentroid(0,0)).toEqual(0)
-    expect(load.partialCentroid(0,10)).toBeCloseTo(5.441)
+    expect(load.partialCentroid(0,0.5)).toBeCloseTo(1/3)
+    expect(load.partialCentroid(0,1)).toBeCloseTo(2/3)
+    expect(load.partialCentroid(1,1)).toEqual(0)
+  })
+
+  it('calculates partial force correctly', () => {
+    expect(load.partialForce(0,0)).toEqual(0)
+    expect(load.partialForce(0,0.5)).toBeCloseTo(0.125)
+    expect(load.partialForce(0,1)).toBeCloseTo(0.50)
+    expect(load.partialForce(1,1)).toEqual(0)
+  })
+})
+
+describe("Descendent uniform Distributed Load", () => {
+  const load = new DistributedLoad(10,5,-5,15)
+
+  it('calculates partial values correctly', () => {
+    expect(load.valueX(-10)).toBeCloseTo(0)
+    expect(load.valueX(0)).toBeCloseTo(8.75)
+    expect(load.valueX(3)).toBeCloseTo(8)
+    expect(load.valueX(6)).toBeCloseTo(7.25)
+    expect(load.valueX(11)).toBeCloseTo(6)
+  })
+
+  it('calculates partial centroid correctly', () => {
+    expect(load.partialCentroid(3,6)).toBeCloseTo(4.4766)
+  })
+
+  it('calculates partial force correctly', () => {
+    expect(load.partialForce(3,6)).toBeCloseTo(22.875)
+  })
+})
+
+describe("Small Uniform Distributed Load", () => {
+  const load = new DistributedLoad(5, 5, 5, 10)
+
+  it('calculates partial values correctly', () => {
+    expect(load.valueX(0)).toBeCloseTo(0)
+    expect(load.valueX(5)).toBeCloseTo(5)
+    expect(load.valueX(10)).toBeCloseTo(5)
+    expect(load.valueX(15)).toBeCloseTo(0)
+  })
+
+  it('calculates partial centroid correctly', () => {
+    expect(load.partialCentroid(0,5)).toBeCloseTo(0)
+    expect(load.partialCentroid(0,10)).toBeCloseTo(7.5)
+    expect(load.partialCentroid(0,20)).toBeCloseTo(7.5)
+    expect(load.partialCentroid(6,6.7)).toBeCloseTo(6.35)
+  })
+
+  it('calculates partial force correctly', () => {
+    expect(load.partialForce(-2,13)).toBeCloseTo(25)
   })
 })

faed0dbaa1e37c08d2182b272c463f1b2c05026e
pitossomo <pedro.h.teles@hotmail.com>
2022-10-09 13:21:56 -0300

    Add test for 2 loads on the same edge

--- src/tests/BeamV2.test.ts
+++ src/tests/BeamV2.test.ts
@@ -153,3 +153,42 @@ describe('Hyperstatic beam with 2 loads overlapping', () => {
     expect(beam.bendingMoment(2)).toBeCloseTo(0)
   })
 })
+
+describe('Hyperstatic beam with 2 loads on the same edge', () => {
+  const distLoad1 = new DistributedLoad(5,5,0,1.5)
+  const distLoad2 = new DistributedLoad(5,5,1.5,2)
+  
+  const nodes = [
+    new Node(0, true),
+    new Node(1, true),
+    new Node(2, true)
+  ]
+
+  const beam = new Beam(nodes, [distLoad1, distLoad2])
+  it('solves for correct reactions', () => {
+    const expectedReactions = [1.875, 6.250, 1.875]
+    expectedReactions.forEach((val, i) => {
+      expect(beam.reactions[i]).toBeCloseTo(val)
+    })
+  })
+
+  it('calculates shear forces correctly', () => {
+    const dx = 0.00000001
+    expect(beam.shearForce(0+dx)).toBeCloseTo(1.875)
+    expect(beam.shearForce(1-dx)).toBeCloseTo(-3.125)
+    expect(beam.shearForce(1+dx)).toBeCloseTo(3.125)
+    expect(beam.shearForce(1.5-dx)).toBeCloseTo(0.625)
+    expect(beam.shearForce(1.5+dx)).toBeCloseTo(0.625)
+    expect(beam.shearForce(2-dx)).toBeCloseTo(-1.875)
+  })
+
+  it('calculates bending moments correctly', () => {
+    const dx = 0.00000001
+    expect(beam.bendingMoment(0)).toBeCloseTo(0)
+    expect(beam.bendingMoment(0.37)).toBeCloseTo(0.352)
+    expect(beam.bendingMoment(1)).toBeCloseTo(-0.625)
+    expect(beam.bendingMoment(1.5-dx)).toBeCloseTo(-0.31)
+    expect(beam.bendingMoment(1.63)).toBeCloseTo(0.352)
+    expect(beam.bendingMoment(2)).toBeCloseTo(0)
+  })
+})

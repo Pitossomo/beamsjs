b63471163e98175fe6f8ee0bea079eb39e88df18
pitossomo <pitossomo@gmail.com>
2023-02-04 10:40:16 -0300

    Fix value's units for the neutral line tests

--- src/tests/NeutralLine.test.ts
+++ src/tests/NeutralLine.test.ts
@@ -1,17 +1,17 @@
-import { Beam, PunctualLoad, DistributedLoad, Node } from "../index"
+import { Beam, DistributedLoad, Node } from "../index"
 
 describe('Isostatic beam', () => {
   const distributedLoads = [
-    new DistributedLoad(2.5, 2.5, 0, 4),
+    new DistributedLoad(10)
   ]
   const nodes = Node.createFixNodes([0, 4])
   const beam = new Beam(nodes, distributedLoads, [], 1)
 
   it('has correct bendingMoment in middle of section', () => {
-    expect(beam.bendingMoment(2)).toBeCloseTo(20000000)
+    expect(beam.bendingMoment(2)).toBeCloseTo(20)
   })
 
   it('has correct neutral line in middle of section', () => {
-    expect(beam.neutralLine(2, 0.14, 0.364, 1.4, 30000000)).toBeCloseTo(0.061)
+    expect(beam.neutralLine(2, 0.14, 0.364, 1.4, 20000/1.4)).toBeCloseTo(0.061)
   })
 })
